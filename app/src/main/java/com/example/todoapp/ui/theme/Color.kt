d2fd9e0e9250503f6580c1386f2f5a26f5a0a4e8
Thitna Damtew <thitnadamtew@gmail.com>
2025-05-18 21:40:31 +0300

    Update Color.kt

--- app/src/main/java/com/example/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/Color.kt
@@ -1,27 +1,31 @@
 package com.example.todoapp.ui.theme
-
 import androidx.compose.ui.graphics.Color
+val LightPrimary = Color(0xFF6750A4)
+val LightSecondary = Color(0xFFE8DEF8)
+val LightTertiary = Color(0xFF7D5260)
+val LightBackground = Color(0xFFFFFBFE)
+val LightSurface = Color(0xFFFFFBFE)
+val LightOnPrimary = Color.White
+val LightOnSecondary = Color.Black
+val LightOnTertiary = Color.White
+val LightOnBackground = Color(0xFF1C1B1F)
+val LightOnSurface = Color(0xFF1C1B1F)
+
+
+val DarkPrimary = Color(0xFFD0BCFF)
+val DarkSecondary = Color(0xFF4A4458)
+val DarkTertiary = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF1C1B1F)
+val DarkSurface = Color(0xFF1C1B1F)
+val DarkOnPrimary = Color.Black
+val DarkOnSecondary = Color.White
+val DarkOnTertiary = Color.Black
+val DarkOnBackground = Color.White
+val DarkOnSurface = Color.White
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-val LightPrimary = Color(0xFF00BCD4)
-val LightSecondary = Color(0xFF03DAC6)
-val LightBackground = Color(0xFFFFFFFF)
-val LightSurface = Color(0xFFFFFFFF)
-val LightOnPrimary = Color.White
-val LightOnSecondary = Color.Black
-val LightOnBackground = Color.Black
-val LightOnSurface = Color.Black
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF6750A4)
+val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
-val DarkPrimary = Color(0xFF00ACC1)
-val DarkSecondary = Color(0xFF03DAC6)
-val DarkBackground = Color(0xFF121212)
-val DarkSurface = Color(0xFF1E1E1E)
-val DarkOnPrimary = Color.Black
-val DarkOnSecondary = Color.Black
-val DarkOnBackground = Color.White
-val DarkOnSurface = Color.White
